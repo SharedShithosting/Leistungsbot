f53b3aae6713e2d29a08df62d4ac38072ae2291d
mikewzr <wurzy96@gmail.com>
2025-01-06 23:03:10 +0100

    Add leistungstag date preselect state

--- leistungsbot/Conversation.py
+++ leistungsbot/Conversation.py
@@ -4,5 +4,6 @@ from enum import IntEnum, auto
 class ConversationState(IntEnum):
     HISTORY_SELECT_KIND = auto()
     LEISTUNGSPOLL_SELECT_LOCATION = auto()
+    LEISTUNGSPOLL_PRESELECT_DATE = auto()
     LEISTUNGSPOLL_SELECT_DATE = auto()
     LEISTUNGSPOLL_PREVIEW = auto()
