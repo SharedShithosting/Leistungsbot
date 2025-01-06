0f6b2097b2bd318794e2d92d324b7ed1061f7c57
mikewzr <wurzy96@gmail.com>
2025-01-06 23:03:54 +0100

    Add specific user context

--- leistungsbot/LeistungbotContext.py
+++ leistungsbot/LeistungbotContext.py
@@ -1,11 +1,26 @@
+from enum import IntEnum
 from typing import TypedDict
+from datetime import datetime
+
 from leistungsbot.Bot import LeistungsBot
 
 from telegram.ext import CallbackContext, ExtBot
 
+class LeistungstagKind(IntEnum):
+    NORMAL = 1
+    KONKURENZ = 2
+    ZUSATZ = 3
+
+class Leistungstag:
+    kind: LeistungstagKind
+    location_id: int
+    datetime: datetime
+
+class UserContext(TypedDict):
+    leistungstag: Leistungstag
 
 class BotContext(TypedDict):
     oldlb: LeistungsBot
 
-class LeistungsbotContext(CallbackContext[ExtBot, dict, dict, BotContext]):
+class LeistungsbotContext(CallbackContext[ExtBot, UserContext, dict, BotContext]):
     pass
